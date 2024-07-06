a4a35f39fb40d59842664920fb5306b006549893
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-07-06 18:30:36 -0400

    sqlite

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using assignment4.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.EntityFrameworkCore.Sqlite;
 
 namespace assignment4
 {
@@ -14,7 +14,7 @@ namespace assignment4
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(connectionString));
+                options.UseSqlite(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)

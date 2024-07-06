7e54af3c7de823859009ad943ff5296000a4d5cf
Zach Stumpf <90022855+zgstumpf@users.noreply.github.com>
2024-07-06 19:08:07 -0400

    role seeding

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace assignment4
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -49,7 +49,28 @@ namespace assignment4
                 pattern: "{controller=Home}/{action=Index}/{id?}");
             app.MapRazorPages();
 
+            await SeedRoles(app);
+
             app.Run();
+        }
+
+        private static async Task SeedRoles(IHost app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                string[] roleNames = { "Student", "Teacher" };
+                IdentityResult roleResult;
+
+                foreach (var roleName in roleNames)
+                {
+                    var roleExist = await roleManager.RoleExistsAsync(roleName);
+                    if (!roleExist)
+                    {
+                        roleResult = await roleManager.CreateAsync(new IdentityRole(roleName));
+                    }
+                }
+            }
         }
     }
 }
